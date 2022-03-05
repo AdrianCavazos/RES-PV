d791f4a7e4b9d8152edbbd462669d8f35a06485d
Juan Soto <47441629+JuanSoto30@users.noreply.github.com>
2022-03-05 16:52:13 -0600

    Update forgot-password.php

--- forgot-password.php
+++ forgot-password.php
@@ -39,26 +39,26 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
-                                        <p class="mb-4">We get it, stuff happens. Just enter your email address below
-                                            and we'll send you a link to reset your password!</p>
+                                        <h1 class="h4 text-gray-900 mb-2">¿Olvidaste tu contraseña?</h1>
+                                        <p class="mb-4">Ingresa to correo electronico para enviar un enlace y puedas recuperar tu 
+                                        contraseña</p>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                                placeholder="Ingresa tu correo electronico...">
                                         </div>
                                         <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                            Reset Password
+                                            Restablecer contraseña
                                         </a>
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="register.html">Crear cuenta</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="login.html">Already have an account? Login!</a>
+                                        <a class="small" href="login.html">¿Ya tienes una cuenta? ¡Accesa!</a>
                                     </div>
                                 </div>
                             </div>
